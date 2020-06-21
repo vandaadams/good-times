bc389a80112705d965ec59fc8cd2837bdfa0361c
Vanda Adams <vandamadams@gmail.com>
2020-06-21 22:58:27 +0100

    header to navbar on scroll down

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// insert header content into desktop header
+
+const header = document.querySelector('.header')
+const desktopHeader = document.querySelector('.header-desktop')
+desktopHeader.innerHTML = header.innerHTML
+
+// hide desktop header when header enters viewport and shows when it leaves
+
+inView('.header')
+  .on('enter', el => desktopHeader.classList.remove('visible'))
+  .on('exit', el => desktopHeader.classList.add('visible'))
